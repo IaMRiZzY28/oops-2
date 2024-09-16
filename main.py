70efeddefc91f7c48686ef923a2e40be5a037871
IaMRiZzY28 <28ragavn@gmail.com>
2024-09-16 18:12:39 +0530

    hup

---
+++ main.py
@@ -0,0 +1,9 @@
+class areaofrectangle():
+    def __init__(self,l,w):
+        self.length=l
+        self.width=w
+    def hibdiskibidij(self):
+        return self.length*self.width
+tanjiro=areaofrectangle(1,9999)
+print("lengthis %d bredthis %d" %(tanjiro.length,tanjiro.width))
+print("areaoflength",tanjiro.hibdiskibidij())
